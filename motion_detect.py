4bfe4087d625c24e22bcd3d71e8ca8ce2cbe443d
Leonardo Guedez <leolespaul@gmail.com>
2019-04-07 15:46:40 -0400

    Adds motion detection module

---
+++ motion_detect.py
@@ -0,0 +1,68 @@
+import numpy as np
+import cv2
+
+WALK_MIN = 21
+WALK_MAX = 30
+
+RUN_MIN = 27
+RUN_MAX = 38
+
+
+def is_walking(x):
+    return WALK_MIN <= x <= WALK_MAX
+
+
+def is_running(x):
+    return RUN_MIN <= x <= RUN_MAX
+
+
+def dist_map(frame1, frame2):
+    """outputs pythagorean distance between two frames"""
+    frame1_32 = np.float32(frame1)
+    frame2_32 = np.float32(frame2)
+    diff32 = frame1_32 - frame2_32
+
+    norm32 = np.sqrt(diff32[:, :, 0]**2 + diff32[:, :, 1] **
+                     2 + diff32[:, :, 2]**2)/np.sqrt(255**2 + 255**2 + 255**2)
+    dist = np.uint8(norm32*255)
+    return dist
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    frame1 = cap.read()[1]
+    frame2 = cap.read()[1]
+
+    while True:
+        frame3 = cap.read()[1]
+
+        dist = dist_map(frame1, frame3)
+
+        frame1 = frame2
+        frame2 = frame3
+
+        # apply Gaussian smoothing
+        mod = cv2.GaussianBlur(dist, (9, 9), 5)
+
+        # calculate st dev test
+        std_dev = cv2.meanStdDev(mod)[1]
+
+        cv2.putText(
+            frame2, f"Standard Deviation - {round(std_dev[0][0], 0)}", (70, 70),
+            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+        if is_walking(std_dev):
+            print('CAM')
+        elif is_running(std_dev):
+            print('CORR')
+
+        cv2.imshow('frame', frame2)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
