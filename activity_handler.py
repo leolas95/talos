f9988df7ca0980e5f6f5f782c4afc644688dcf21
Leonardo Guedez <leolespaul@gmail.com>
2019-04-08 16:36:14 -0400

    Adds requirements file and fixex bugs in config file loading.

--- activity_handler.py
+++ activity_handler.py
@@ -5,16 +5,17 @@ def handle_activity(activity, activities_conditions, frame):
     cv2.putText(frame, activity.capitalize(), (30, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
 
-    conditions = activities_conditions[:]
-    for condition in conditions:
-        if activity != condition['activity']:
-            continue
+    if activities_conditions is not None:
+        conditions = activities_conditions[:]
+        for condition in conditions:
+            if activity != condition['activity']:
+                continue
 
-        action = condition['action']
-        action_args = condition['action_arguments']
+            action = condition['action']
+            action_args = condition['action_arguments']
 
-        if action in actions.ACTIONS_THAT_REQUIRE_FRAME:
-            action_args.append(frame)
+            if action in actions.ACTIONS_THAT_REQUIRE_FRAME:
+                action_args.append(frame)
 
-        actions.do(action, *action_args)
-        activities_conditions.remove(condition)
+            actions.do(action, *action_args)
+            activities_conditions.remove(condition)
--- config_file_loader.py
+++ config_file_loader.py
@@ -23,10 +23,45 @@ def load(filename):
             result = xmltodict.parse(file.read())
             result = dict(result['root'])
             result = json.loads(json.dumps(result))
-            if result['targets_conditions'] is None:
-                result['targets_conditions'] = []
-                
-            if result['activities_conditions'] is None:
-                result['targets_conditions'] = []
+
+            # xmltodict includes some garbage when parsing lists, so we still have to
+            # manually parse input (sigh...)
+
+            # If there are targets with empty properties, replace None value with {}
+            if result.get('targets'):
+                for target, targetval in result['targets'].items():
+                    if targetval is None:
+                        result['targets'][target] = {}
+
+            # If there are activities with empty values, replace None with {}
+            if result.get('activities'):
+                for activity in result['activities'].keys():
+                    if result['activities'][activity] is None:
+                        result['activities'][activity] = {}
+
+            # Transform objects with 'item' key to a list of those objects
+            if result.get('activities_conditions'):
+                result['activities_conditions'] = result['activities_conditions']['item']
+
+                if type(result['activities_conditions']).__name__ != 'list':
+                    result['activities_conditions'] = [
+                        result['activities_conditions']]
+
+                for condition in result['activities_conditions']:
+                    if type(condition['action_arguments']['item']).__name__ != 'list':
+                        condition['action_arguments'] = [
+                            condition['action_arguments']['item']]
+
+            if result.get('targets_conditions'):
+                result['targets_conditions'] = result['targets_conditions']['item']
+
+                if type(result['targets_conditions']).__name__ != 'list':
+                    result['targets_conditions'] = [
+                        result['targets_conditions']]
+
+                for condition in result['targets_conditions']:
+                    if type(condition['action_arguments']['item']).__name__ != 'list':
+                        condition['action_arguments'] = [
+                            condition['action_arguments']['item']]
 
     return result
