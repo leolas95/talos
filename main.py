76a9561400a4b360243981a213a42e194b667901
Leonardo Guedez <leolespaul@gmail.com>
2019-04-11 14:16:08 -0400

    Adjusts program to allow multiple targets of the same class.
    It adds another loop to iterate over all the targets, acording to the
    new format of the config file. Also adds colors for the rest of the
    target classes.

--- main.py
+++ main.py
@@ -17,14 +17,15 @@ from targets.tracker.centroidtracker import CentroidTracker
 from targets.conditions.check import check_targets_conditions
 
 # construct the argument parse and parse the arguments
-argument_parser = argparse.ArgumentParser()
-argument_parser.add_argument("-c", "--confidence", type=float, default=0.2,
+ARGUMENT_PARSER = argparse.ArgumentParser()
+ARGUMENT_PARSER.add_argument("-c", "--confidence", type=float, default=0.25,
                              help="minimum probability to filter weak detections")
 
-argument_parser.add_argument("-f", "--file", type=str, default="config.json",
+ARGUMENT_PARSER.add_argument("-f", "--file", type=str, default="config.json",
                              help="Name of the configuration file. Defaults to 'config.json' \
                 in the directory where the main script is located")
-args = vars(argument_parser.parse_args())
+
+ARGUMENTS = vars(ARGUMENT_PARSER.parse_args())
 
 # initialize the list of class labels MobileNet SSD was trained to
 # detect, then generate a set of bounding box colors for each class
@@ -35,28 +36,34 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 #COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 COLORS = [
-    (100, 120, 100),
-    (90, 178, 30),
-    (11, 33, 57),
-    (23, 42, 13),
-    (12, 99, 44),
-    (55, 98, 90),
-    (100, 100, 200),
+    (0, 0, 0),
+    (90, 178, 30),  # aeroplane
+    (11, 33, 57),  # bicycle
+    (23, 42, 13),  # bird
+    (12, 99, 44),  # boat
+    (55, 98, 90),  # bottle
+    (100, 100, 200),  # bus
     (140, 175, 80),  # car
-    (150, 100, 140),
-    (160, 180, 200),
-    (100, 90, 134),
-    (96, 78, 45),
-    (34, 90, 121),
-    (200, 145, 40),
-    (124, 55, 90),
+    (150, 100, 140),  # cat
+    (250, 0, 140),  # chair
+    (100, 90, 134),  # cow
+    (96, 78, 45),  # diningtable
+    (34, 90, 121),  # dog
+    (200, 145, 40),  # horse
+    (124, 55, 90),  # motorbike
     (223.39221401, 113.79734267, 17.9394392),  # person
+    (60, 88, 44),  # pottedplant
+    (99, 170, 115),  # sheep
+    (190, 55, 200),  # sofa
+    (160, 44, 190),  # train
+    (112, 113, 56),  # tvmonitor
 ]
 
 CURRENT_DATE_FORMAT_STRING = "%A %d %B %Y %I:%M:%S %p"
 
+
 def main():
-    program_data = config_file_loader.load(args['file'])
+    program_data = config_file_loader.load(ARGUMENTS['file'])
 
     targets = program_data.get('targets')
     if targets is not None:
@@ -109,7 +116,8 @@ def main():
         if activities is not None:
             activity = detect_activity(frame1, frame2, frame)
             if activity is not None and activity in activities:
-                check_activity_conditions(activity, activities_conditions, frame)
+                check_activity_conditions(
+                    activity, activities_conditions, frame)
 
         # grab the frame dimensions and convert it to a blob
         (h, w) = frame.shape[:2]
@@ -141,7 +149,7 @@ def main():
 
             # filter out weak detections by ensuring the `confidence` is
             # greater than the minimum confidence
-            if confidence > args["confidence"]:
+            if confidence > ARGUMENTS["confidence"]:
                 # extract the index of the class label from the
                 # `detections`, then compute the (x, y)-coordinates of
                 # the bounding box for the object
@@ -176,57 +184,59 @@ def main():
         for (object_id, (centroid, rect)) in objects.items():
             (x1, y1, x2, y2, (p1, p2, label, labely, color, target_name)) = rect
 
-            minimum = program_data['targets'][target_name].get('min')
-            maximum = program_data['targets'][target_name].get('max')
-            detected_objects = class_counter.get(target_name)
-            properties = program_data['targets'][target_name].get('properties')
+            for target in program_data['targets'][target_name]:
+                minimum = target.get('min')
+                maximum = target.get('max')
+                detected_objects = class_counter.get(target_name)
+                properties = target.get('properties')
 
-            # Name of the counter specified by the user in the DSL program
-            counter_name = program_data['targets'][target_name].get('counter')
+                # Name of the counter specified by the user in the DSL program
+                counter_name = target.get('counter')
 
-            if detected_objects is None:
-                continue
+                if detected_objects is None:
+                    continue
 
-            class_counter[target_name] -= 1
+                class_counter[target_name] -= 1
 
-            if targets_conditions is not None:
-                # Check if some condition holds true
-                check_targets_conditions(targets_conditions, counters, frame)
+                if targets_conditions is not None:
+                    # Check if some condition holds true
+                    check_targets_conditions(
+                        targets_conditions, counters, frame)
 
-            object_data = {
-                'bounding_box': (x1, y1, x2, y2),
-                'middle_line_coords': (p1, p2),
-                'color': color,
-                'label': label,
-                'labely': labely,
-                'object_id': object_id,
-                'centroid': centroid,
-                'counter_name': counter_name,
-            }
+                object_data = {
+                    'bounding_box': (x1, y1, x2, y2),
+                    'middle_line_coords': (p1, p2),
+                    'color': color,
+                    'label': label,
+                    'labely': labely,
+                    'object_id': object_id,
+                    'centroid': centroid,
+                    'counter_name': counter_name,
+                }
 
-            # Specified both minimum and maximum amount of objects
-            if minimum is not None and maximum is not None:
-                if minimum <= detected_objects <= maximum:
+                # Specified both minimum and maximum amount of objects
+                if minimum is not None and maximum is not None:
+                    if minimum <= detected_objects <= maximum:
+                        check_target_properties(
+                            frame, properties, counters, object_data)
+
+                # Just minimum
+                elif minimum is not None and maximum is None:
+                    if detected_objects >= minimum:
+                        check_target_properties(
+                            frame, properties, counters, object_data)
+
+                # Just maximum
+                elif minimum is None and maximum is not None:
+                    if detected_objects <= maximum:
+                        check_target_properties(
+                            frame, properties, counters, object_data)
+
+                # Neither minimum nor maximum
+                else:
+                    # So just check properties onwards
                     check_target_properties(
                         frame, properties, counters, object_data)
-
-            # Just minimum
-            elif minimum is not None and maximum is None:
-                if detected_objects >= minimum:
-                    check_target_properties(
-                        frame, properties, counters, object_data)
-
-            # Just maximum
-            elif minimum is None and maximum is not None:
-                if detected_objects <= maximum:
-                    check_target_properties(
-                        frame, properties, counters, object_data)
-
-            # Neither minimum nor maximum
-            else:
-                # So just check properties onwards
-                check_target_properties(
-                    frame, properties, counters, object_data)
 
         # show the current date on the bottom right corner
         current_date = datetime.datetime.now().strftime(CURRENT_DATE_FORMAT_STRING)
@@ -251,7 +261,6 @@ def main():
     # do a bit of cleanup
     cv2.destroyAllWindows()
     video_source.stop()
-    print(counters)
 
 
 if __name__ == '__main__':
